1ac86dce7c81e9e02b554621120dcbd619ada432
busdmv@gmail.com <Teamiiko13>
2023-04-28 19:31:10 +0300

    Main.java создание класса в котором происходит создание виртуального сервера LinkResource.java класс с простым хранилищем ссылок. Реализация простых Get и Put методов

---
+++ src/main/java/Main.java
@@ -0,0 +1,30 @@
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
+import org.glassfish.jersey.server.ResourceConfig;
+
+import java.io.IOException;
+import java.net.URI;
+
+public class Main {
+    // Base URI the Grizzly HTTP server will listen on
+    public static final String BASE_URI = "http://localhost:8080/api";
+
+    /**
+     * Starts Grizzly HTTP server exposing JAX-RS links.resources defined in this application.
+     *
+     * @return Grizzly HTTP server.
+     */
+    public static HttpServer startServer() {
+        final ResourceConfig rc = new ResourceConfig().packages("resources");
+
+        return GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI), rc);
+    }
+
+    public static void main(String[] args) throws IOException {
+        final HttpServer server = startServer();
+        System.out.printf("Jersey app started with WADL available at "
+                + "%s application.wadl\nHit enter to stop it...%n", BASE_URI);
+        System.in.read();
+        server.shutdownNow();
+    }
+}
